38071780cff661942e8bf77ea06089e26e11203d
Jeremiah Smith <TheButtonBreaker@outlook.com>
2017-09-14 12:55:07 -0700

    Add files via upload

---
+++ http/index.php
@@ -0,0 +1,52 @@
+<html>
+	<link rel="stylesheet" type="text/css">
+	<head><title> Jeremiah's HTML </title></head>
+	<body style="background-color: #C8C8C8;">
+CBC HTML (Hyper Text Markup Language)<br />
+<div>
+<ul>
+<li><a href="index.php"> Home</a> &nbsp;</li>
+<li><a href="contact.html"> Contact</a> &nbsp;</li>
+<li><a href="documents/" target="_blank"> Documents</a> &nbsp;</li>
+<li><a href="http://thetypingcat.com/" target="_blank"> TheTypingCat</a> &nbsp;</li>
+<li><a href="https://github.com/Pearlhat23" target="_blank"> Git-Hub</a> &nbsp;</li>
+<li><a href="html/" target="_blank"> HTML</a> &nbsp;</li>
+<li><a href="logs/" target="_blank"> Logs</a> &nbsp;</li>
+</ul>
+</div>
+<style>
+ ul {
+    list-style-type: none;
+    overflow: hidden;
+    background-color: #333;
+}
+
+ li {
+    float: left;
+    background-color: #333;
+}
+
+ li a {
+    display: block;
+    color: blue;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    background-color: #333;
+    font-size: 24px;
+    text-shadow: 1px 1px #ff0000;
+}
+
+/* Change the link color to #111 (black) on hover */
+ li a:hover {
+    background-color: #404040;
+}
+</style>
+
+
+<?php
+echo "";
+phpinfo();
+?>
+</body>
+</html>
